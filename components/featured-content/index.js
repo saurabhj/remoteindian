630472451ff51a54fb9467a0c2892f324827a504
Abhishek Bose <abhishekbose87@gmail.com>
2020-06-01 12:33:35 +0530

    Get a sample version working locally

--- components/featured-content/index.js
+++ components/featured-content/index.js
@@ -1,10 +1,9 @@
 import { FeaturedWrap } from './style';
 import FeaturedGuides from './guides';
-import FeaturedRoadmaps from './roadmaps';
+// import FeaturedRoadmaps from './roadmaps';
 
 const FeaturedContent = (props) => (
   <FeaturedWrap className="border-top bg-light">
-    <FeaturedRoadmaps />
     <FeaturedGuides />
   </FeaturedWrap>
 );
---
+++ next.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const fs = require('fs');
+const withSass = require('@zeit/next-sass');
+const withCSS = require('@zeit/next-css');
+// const rehypePrism = require('@mapbox/rehype-prism');
+const withMDX = require('@next/mdx')({
+  extension: /\.(md|mdx)?$/,
+});
+
+const { getPathMap } = require("./scripts/path-map");
+
+/**
+ * Loads the configuration for the given environment
+ * @param env
+ * @returns {*}
+ */
+const loadConfig = (env = 'dev') => {
+  const configPath = `./config/${env}.json`;
+  if (!fs.existsSync(configPath)) {
+    console.log(`Config file not found: ${configPath}`);
+    process.exit(1);
+  }
+
+  console.log(`Config file found: ${configPath}`);
+
+  // @todo stringify the values for webpack - it doesn't understand objects
+  return require(configPath);
+};
+
+const options = {
+  exportPathMap: getPathMap(),
+  env: loadConfig(process.env.NODE_ENV),
+  pageExtensions: ['jsx', 'js', 'mdx', 'md'],
+  webpack(config, options) {
+    // // Transforms SVGs to components
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+  config.resolve.modules.push(path.resolve('./'));
+
+    // Allow loading images and fonts
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+        },
+      },
+    });
+
+    return config
+  },
+};
+
+let nextConfig = withSass(options);
+nextConfig = withCSS(nextConfig);
+nextConfig = withMDX(nextConfig);
+
+module.exports = nextConfig;
